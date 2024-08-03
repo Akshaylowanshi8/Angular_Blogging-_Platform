8f91833e4d1fb43d2314ecd54bfa9336d1dd9fb4
akshay <akshaylovanshi8@gmail.com>
2024-08-03 19:44:26 +0530

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Datas } from './../datas';
+import { BloggingService } from '../blogging.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink ,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+Data : Datas[] = []; 
+constructor (public BloggingService : BloggingService){}
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+
+  this.BloggingService.GetAllUserData().subscribe((data:Datas[])=>{
+this.Data=data;
+console.log(this.Data);
+ })
+}
 
 }
+
+
+
+
